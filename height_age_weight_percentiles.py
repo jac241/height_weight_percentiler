5b9cba080b1cac8c171d1eec34dc2e5265ff0274
James Castiglione <jamescastiglione@Jamess-Mac-mini.home>
2019-02-09 16:03:11 -0500

    Calculates percentiles for children < 24 months

---
+++ height_age_weight_percentiles.py
@@ -0,0 +1,96 @@
+import os
+import pandas as pd
+import csv
+import numpy as np
+import scipy.stats as stats
+
+nsqip = '~/research/PNSQIP_CPT_abbreviated.xlsx'
+wtage = os.path.expanduser('./wtageinf.xls')
+
+POUNDS_TO_KG = 2.20462262185
+
+def main():
+    nsqip_xl = pd.ExcelFile(nsqip)
+    nsqip_df = nsqip_xl.parse('Sheet1')
+    nsqip_df = nsqip_df[(nsqip_df.AGE_DAYS < (24 * 30))]
+
+    wtage_df = get_cdc_dataframe(wtage, 'wtageinf')
+    add_string_male_female(wtage_df)
+
+    print("Calculating Percentiles...")
+    nsqip_df['wtpercentile'] = calculate_percentiles_for_weight(nsqip_df, wtage_df)
+    return nsqip_df
+    
+def get_cdc_dataframe(path, sheet_name):
+    return pd.ExcelFile(path).parse(sheet_name)
+
+
+def build_percentile_map(path):
+   with open(path) as csvfile:
+       reader = csv.reader(csvfile)
+       skip_header(reader)
+
+       for row in reader:
+           print(', '.join(row))
+    
+
+def skip_header(csvreader):
+    next(csvreader, None)
+
+
+
+def get_nsqip_dataframe(path):
+    nsqip_xl = pd.ExcelFile(nsqip)
+    nsqip_df = nsqip_xl.parse('Sheet1')
+    return nsqip_df
+
+
+def age_in_days_to_months(age_in_days):
+    return age_in_days / (365.25/12)
+
+
+def add_string_male_female(df):
+    df['Sex_str'] = df.Sex.apply(lambda s: 'Male' if s == 1 else 'Female')
+
+
+def variable_for_age_in_months_and_sex(cdc_df, variable, age_in_mos, sex):
+    if age_in_mos == 0:
+        return cdc_df[variable][0]
+    else:
+        age_less_than = cdc_df[(np.floor(cdc_df.Agemos) <= np.floor(age_in_mos)) & (cdc_df.Sex_str == sex)]
+        return age_less_than[variable].iloc[-1]
+
+
+def zscore_for_age_in_months_and_sex(cdc_df, measurement, age_in_mos, sex):
+    L = variable_for_age_in_months_and_sex(cdc_df, 'L', age_in_mos, sex)
+    M = variable_for_age_in_months_and_sex(cdc_df, 'M', age_in_mos, sex)
+    S = variable_for_age_in_months_and_sex(cdc_df, 'S', age_in_mos, sex)
+    return zscore_for_measurement(measurement, L, M, S)
+
+
+def zscore_for_measurement(measurement, L, M, S):
+    return ((measurement/M)**(L) - 1) / (L*S)
+    
+
+def percentile_for_zscore(zscore):
+    return stats.norm.cdf(zscore)
+
+
+def calculate_percentiles_for_weight(nsqip_df, cdc_df):
+    def percentile_per_row(row):
+        if row['WEIGHT'] < 0:
+            return row['WEIGHT']
+        else:
+            return percentile_for_zscore(
+                zscore_for_age_in_months_and_sex(
+                    cdc_df,
+                    row['WEIGHT'] / POUNDS_TO_KG,
+                    age_in_days_to_months(row['AGE_DAYS']),
+                    row['SEX']
+                )
+            )
+
+    return nsqip_df.apply(percentile_per_row, axis=1)
+
+if __name__ == '__main__':
+    nsqip_df = main()
